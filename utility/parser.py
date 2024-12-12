875f7d174250974759518123ec5084b0681eb0a4
Pham Linh <ltp8@zoo.cs.yale.edu>
2024-12-12 01:45:34 -0500

    defaults updated

--- utility/parser.py
+++ utility/parser.py
@@ -32,15 +32,15 @@ python Main.py --model_type kgat --alg_type bi --dataset amazon-book
                         help='CF Embedding size.')
     parser.add_argument('--lr', type=float, default=0.0001,
                         help='Learning rate.')
-    parser.add_argument('--epoch', type=int, default=100, #paper uses 1000 epochs, but we don't have enough time
+    parser.add_argument('--epoch', type=int, default=10, #paper uses 1000 epochs, but we don't have enough time
                         help='Number of epoch.')
-    parser.add_argument('--verbose', type=int, default=50,
+    parser.add_argument('--verbose', type=int, default=1,
                         help='Interval of evaluation.')
     parser.add_argument('--save_flag', type=int, default=1,
                         help='0: Disable model saver, 1: Activate model saver')
     parser.add_argument('--pretrain', type=int, default=-1,
                         help='0: No pretrain, -1: Pretrain with the learned embeddings, 1:Pretrain with stored models.')
-    parser.add_argument('--batch_size', type=int, default=128,
+    parser.add_argument('--batch_size', type=int, default=16,
                         help='CF batch size.')
     parser.add_argument('--node_dropout', nargs='?', default='[0.1]',
                         help='Keep probability w.r.t. node dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
@@ -63,7 +63,7 @@ python Main.py --model_type kgat --alg_type bi --dataset amazon-book
                         help='Project path.')
     parser.add_argument('--kge_size', type=int, default=64,
                         help='KG Embedding size.')
-    parser.add_argument('--batch_size_kg', type=int, default=128,
+    parser.add_argument('--batch_size_kg', type=int, default=16,
                         help='KG batch size.')
     parser.add_argument('--adj_type', nargs='?', default='si',
                         help='Specify the type of the adjacency (laplacian) matrix from {bi, si}.')

21bf420c4c6b0170b9e406ecfe369f3ef2f63374
Yang Kerui <ky298@zoo.cs.yale.edu>
2024-12-13 10:58:24 -0500

    three hop update

--- utility/KGAT.py
+++ utility/KGAT.py
@@ -442,9 +442,9 @@ class KGAT(object):
         elif self.hop == "three": 
             alpha = 0.25 
             two_hop = tf.sparse.sparse_dense_matmul(A, tf.sparse.to_dense(A))
-            three_hop = tf.sparse.sparse_dense_matmul(two_hop, tf.sparse.to_dense(A))
-            A = tf.sparse.add(A_identity,
-                alpha * (1 - alpha) * tf.sparse.to_dense(A) + alpha * (1 - alpha)**2 * two_hop + alpha * (1-alpha)**3 * three_hop
+            three_hop = tf.sparse.sparse_dense_matmul(tf.sparse.from_dense(two_hop), tf.sparse.to_dense(A))
+            A = tf.sparse.add(A,
+                alpha * tf.sparse.to_dense(A) + alpha * (1 - alpha)**2 * two_hop + alpha * (1-alpha)**3 * three_hop
             )
             print('calculated 3-hop attention')
         else: 
